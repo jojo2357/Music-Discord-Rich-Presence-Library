0c97ea00b6439d01c1eba3c95b2f9d6868f3db6b
RoguedBear <39442192+RoguedBear@users.noreply.github.com>
2021-03-31 10:35:57 +0530

    Github Actions Bot to generate a markdown file of all the albums added in the MDRP Library (finally 🥳 ) (#12)
    * added the python script which generates a markdown of all the albums currently added in the MDRP
    
    * add the action yaml
    
    * corrected syntax error. hopefully work now
    
    * manually adding markdown file to git
    
    * manually adding doesn't work. trying `.` file pattern (default)
    
    * ignore pycache files
    
    * escaping all special characters in artist and album names to prevent unintentional markdown formatting
    
    * switching branch before running script
    * only for testing purpose
    * attempt to fix: " The following untracked working tree files would be overwritten by checkout:"
    
    * switch branch before running attempt 2
    
    * abandon testing branch. generate files in current only 😪
    
    * Commit the generated file
    
    * change bot identity
    
    * trivial change to see whether changed in md file are committed or not
    
    * fix bot identity (author)
    
    * Commit the generated file
    
    * update commit message by the bot to be a little less boring
    
    * now also generating a plaintext file of all the songs in MDRP library.
    also adding the plaintext file to git via this commit
    
    * Placed "Unknown Artist" at the end of the file instead of them being in between.
    
    * Update file to reflect new albums in library 🥳
    
    Co-authored-by: Github Actions [bot] <actions@github.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ generate_artists.py
@@ -0,0 +1,140 @@
+import logging
+import os
+import pprint
+from os.path import join, isfile
+from typing import Tuple, Dict
+
+
+def find_all_dat_files():
+    """
+    finds all the dat files in "User_Generated_Files" folder
+    :return:
+    """
+    all_files = []
+
+    for dir_path, folders, file in os.walk("User_Generated_Files"):
+        all_files += [join(dir_path, f) for f in file if isfile(join(dir_path, f))]
+
+    logger.info("Found: %d files", len(all_files))
+
+    return all_files
+
+
+def get_album_and_artist(line: str) -> Tuple[str, str]:
+    """
+    from a `dat` file return the album and artist name from the given line.
+    Returns "Unknown Artist" if artist key is not present
+    :param line: the line containing the MDRP music key
+    :return: 2-item tuple of format <album>, <artist>
+    """
+    splitted = line.split("==")
+
+    # if artist key is not present
+    if len(splitted) == 2:
+        artist = "Unknown Artist"
+    else:
+        artist = splitted[-1].strip("\n")
+
+    # Setting the album name
+    album = splitted[0]
+
+    return album, artist
+
+
+def escape(string: str):
+    """
+    escapes the special characters with a backslash
+    :param string:
+    :return:
+    """
+    escaped_string = ""
+    for letter in string:
+        if not letter.isalnum() and letter != " ":
+            logger.debug("escaping: %s \t %s", letter, string)
+            escaped_string += rf"\{letter}"
+        else:
+            escaped_string += letter
+    return escaped_string
+
+
+if __name__ == '__main__':
+    # Test prints
+    # print(find_all_dat_files())
+    # print(get_album_and_artist("Live from Vatnagarðar==16"))
+    #
+
+    # Set the logger
+    # noinspection PyArgumentList
+    logging.basicConfig(format="[{asctime}] :--{levelname:-^9s}--: [{funcName}()] {message}",
+                        datefmt="%d/%b/%Y %H:%M:%S",
+                        style="{")
+    logger = logging.getLogger()
+    logger.setLevel(10)
+
+    # Get a list of all the dat files
+    ALL_FILES = find_all_dat_files()
+
+    ALL_ALBUMS_WITH_ARTISTS: Dict[str, list] = {"Unknown Artist": []}
+    stat_counter = {"total_songs": 0}
+    # Go through each file.
+    for dat_file in ALL_FILES:
+        with open(dat_file) as f:
+            # Skip the first 2 lines
+            f.readline()
+            f.readline()
+
+            # Now go through each entry and store it in master dictionary
+            for album_entry in f:
+                album, artist = get_album_and_artist(album_entry)
+                # Check if the album's entry for artist exists or not to avoid double entry
+                if album not in ALL_ALBUMS_WITH_ARTISTS.setdefault(artist, []):
+                    ALL_ALBUMS_WITH_ARTISTS[artist].append(album)
+                    stat_counter["total_songs"] += 1
+    logger.debug(pprint.pformat(ALL_ALBUMS_WITH_ARTISTS))
+
+    logger.info("Found and added %d songs", stat_counter["total_songs"])
+
+    # Now format for the markdown entry.
+    MARKDOWN_CONTENT = ""
+
+    # Go through the artist's name in alphabetical order
+    sorted_artists = sorted(ALL_ALBUMS_WITH_ARTISTS.keys(), key=lambda x: x.lower())
+    sorted_artists += [sorted_artists.pop(sorted_artists.index("Unknown Artist"))]
+    for artist in sorted_artists:
+        # Append the artist name with level 2 headings
+        MARKDOWN_CONTENT += f"## {escape(artist)}\n"
+
+        # Now for their albums:
+        for album in sorted(ALL_ALBUMS_WITH_ARTISTS[artist]):
+            # Add the album name with bullet points
+            MARKDOWN_CONTENT += f" - {escape(album)}\n"
+
+    # Now write the Markdown file
+    with open("Albums_in_MDRP.md", "w") as file:
+        file.write("# List of Albums:\n")
+        file.write("This file contains the list of all the songs that are currently added in the MDRP discord"
+                   " applications.\n\n")
+        file.write("---\n")
+
+        file.write(MARKDOWN_CONTENT)
+        logger.info("Created the markdown file")
+
+    # Repeat the above part again cuz.. faszt computers ⚡⚡
+
+    TEXT_CONTENT = ""
+    # Go through the artist's name in alphabetical order
+    for artist in sorted_artists:
+        # Append the artist name with level 2 headings
+        TEXT_CONTENT += f"⁕ {artist}\n"
+
+        # Now for their albums:
+        for album in sorted(ALL_ALBUMS_WITH_ARTISTS[artist]):
+            # Add the album name with bullet points
+            TEXT_CONTENT += f"\t • {album}\n"
+
+        TEXT_CONTENT += "\n\n"
+
+    # Now write the text file
+    with open("Albums_in_MDRP.txt", "w") as file:
+        file.write(TEXT_CONTENT)
+        logger.info("Created the text file")
